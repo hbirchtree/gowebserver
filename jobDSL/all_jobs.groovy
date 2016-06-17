378559836568c3ce15a1ddbc641f3f363e9f811f
marius moe <mariusmoe@users.noreply.github.com>
2016-06-17 13:50:21 +0200

    Update all_jobs.groovy

--- jobDSL/all_jobs.groovy
+++ jobDSL/all_jobs.groovy
@@ -117,7 +117,7 @@ fi
 ''')
 
     shell('''
-    sudo docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 go test -v
+    sudo docker run --rm -v "$WORKSPACE":/usr/src/myapp -w /usr/src/myapp golang:1.6 go test -v
     ''')
 
   }
